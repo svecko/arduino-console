bebf352498dc249c73e35b14e66a9d254df8728d
Miha Svecak <miha.svecak@gmail.com>
2020-02-12 13:29:00 +0100

    Initial commit

--- arduino-console.ino
+++ arduino-console.ino
@@ -4,6 +4,8 @@
 #define DC   8
 #define RESET  9  
 
+int VRx, VRy;
+
 TFT myScreen = TFT(CS, DC, RESET);
 
 void setup() {
@@ -13,12 +15,21 @@ void setup() {
   myScreen.background(0,0,0); // clear the screen
   myScreen.stroke(255,0,255);
   myScreen.setTextSize(2);
-  pinMode(5, OUTPUT);
-  digitalWrite(5, HIGH);
+
+  Serial.begin(9600);
 }
 
 void loop() {
     // print out and erase
     myScreen.stroke(255,255,255);
-    myScreen.text("Display radi",0,0);
+
+    VRx = map(analogRead(A1), 0, 1023, 0, 160);
+    VRy = map(analogRead(A0), 0, 1023, 0, 128);
+
+    Serial.print("VRx: ");
+    Serial.println(VRx);
+    Serial.print("VRy: ");
+    Serial.println(VRy);
+    
+    myScreen.text("Display radi",VRx,VRy);
 }
