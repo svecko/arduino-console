c6174fd6d857c7fd32b751f3741ee0abe273e071
Miha Svecak <miha.svecak@gmail.com>
2020-03-04 13:26:06 +0100

    Updated arduino-console.ino

--- arduino-console.ino
+++ arduino-console.ino
@@ -1,35 +1,90 @@
-#include <TFT.h> // Hardware-specific library
+// Display header
+#include <TFT.h>
 
-#define CS   10
-#define DC   8
-#define RESET  9  
+#define CS 10
+#define DC 8
+#define RESET 9
 
-int VRx, VRy;
+struct Joystick
+{
+  int x, y;
+  int buttonPin, buttonState;
 
+  bool isPressed()
+  {
+    return buttonState == 0 ? true : false;
+  }
+
+  void PrintState()
+  {
+    Serial.print("x: ");
+    Serial.println(x);
+
+    Serial.print("y: ");
+    Serial.println(y);
+
+    Serial.print("button state: ");
+    Serial.println(buttonState == 0 ? "button is pressed" : "button is not pressed");
+  }
+};
+
+struct Point
+{
+  int x, y;
+  int xPrev, yPrev;
+};
+
+// TFT display
 TFT myScreen = TFT(CS, DC, RESET);
 
-void setup() {
+// Joystick
+Joystick joystick;
+Point point;
+joystick.buttonPin = A2;
+
+void setup()
+{
+  // TFT display
   pinMode(5, OUTPUT);
   digitalWrite(5, HIGH);
-  myScreen.begin();  
-  myScreen.background(0,0,0); // clear the screen
-  myScreen.stroke(255,0,255);
-  myScreen.setTextSize(2);
+  myScreen.begin();
+  myScreen.background(0, 0, 0);
 
   Serial.begin(9600);
 }
 
-void loop() {
-    // print out and erase
-    myScreen.stroke(255,255,255);
+void loop()
+{
+  // Get joystick data
+  joystick.x = map(analogRead(A1), 0, 1023, 0, 160);
+  joystick.y = map(analogRead(A0), 1023, 0, 0, 128);
+  joystick.buttonState = analogRead(joystick.buttonPin);
 
-    VRx = map(analogRead(A1), 0, 1023, 0, 160);
-    VRy = map(analogRead(A0), 0, 1023, 0, 128);
+  // Update point position
+  point.x = joystick.x;
+  point.y = joystick.y;
 
-    Serial.print("VRx: ");
-    Serial.println(VRx);
-    Serial.print("VRy: ");
-    Serial.println(VRy);
-    
-    myScreen.text("Display radi",VRx,VRy);
+  // Update previous point position
+  point.xPrev = point.x;
+  point.yPrev = point.y;
+
+  // Clean previous point position
+  if (point.x != point.xPrev || point.y != point.yPrev)
+  {
+    myScreen.stroke(0, 0, 0);
+    myScreen.point(point.xPrev, point.yPrev);
+  }
+
+  if (joystick.isPressed())
+  {
+    // Fill red and display point
+    myScreen.stroke(0, 0, 255);
+    myScreen.point(point.x, point.y);
+  }
+  else
+  {
+    // Fill white and display point
+    myScreen.stroke(255, 255, 255);
+    myScreen.point(point.x, point.y);
+  }
 }
